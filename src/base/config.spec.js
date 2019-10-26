e33ed846ad16ee46e486b7d1d43b44d27b90ae97
Viswanath.Ct <2ndryid@gmail.com>
2019-10-26 15:30:21 +0530

    Introduced a few jest options.

--- src/base/config.spec.js
+++ src/base/config.spec.js
@@ -6,28 +6,33 @@ const mockProcessModule = {
 
 jest.mock('process', () => mockProcessModule);
 
-describe('the config', () => {
-	beforeEach(() => {
-		jest.resetModules();
-	});
+/* Helpers */
+const forceRequire = (module) => {
+	jest.unmock(module);
+	return require(module);
+}
 
+describe('the config', () => {
 	test('The env-var NODE_ENV is mapped to config.env.', () => {
 		mockEnv.NODE_ENV = 'production';
-		const { config } = require('../../src/base/config');
+		const { config } = forceRequire('./config');
 
 		expect(config.environment).toEqual('production');
+		delete mockEnv.NODE_ENV;
 	});
 
 	test('When NODE_ENV is not present config.env is development.', () => {
 		delete mockEnv.NODE_ENV;
-		const { config } = require('../../src/base/config');
+
+		const { config } = forceRequire('./config');
 
 		expect(config.environment).toEqual('development');
 	});
 
 	test('Environment variables are available as config.env.', () => {
 		mockEnv.someVar = Symbol('someVar');
-		const { config } = require('../../src/base/config');
+
+		const { config } = forceRequire('./config');
 
 		expect(config.env).toEqual(mockEnv);
 	});
--- src/base/logger.spec.js
+++ src/base/logger.spec.js
@@ -2,22 +2,23 @@
 const mockConfigModule = {};
 const mockPino = jest.fn();
 
-jest.mock('../../src/base/config', () => ({
+/* Helpers */
+const forceRequire = (module) => {
+	jest.unmock(module);
+	return require(module);
+}
+
+jest.mock('./config', () => ({
 	config: mockConfigModule,
 }));
 jest.mock('pino', () => mockPino);
 
 describe('the logger', () => {
-	beforeEach(() => {
-		jest.resetModules();
-		jest.resetAllMocks();
-	});
-
 	test('When the environment is production the logger'
 		+ ' doesn\'t prettyPrint.', () => {
 		mockConfigModule.environment = 'production';
 
-		require('../../src/base/logger');
+		forceRequire('./logger');
 
 		expect(mockPino).toBeCalledWith({
 			prettyPrint: false,
@@ -28,7 +29,7 @@ describe('the logger', () => {
 		+ ' does prettyPrint.', () => {
 		mockConfigModule.environment = 'development';
 
-		require('../../src/base/logger');
+		forceRequire('./logger');
 
 		expect(mockPino).toBeCalledWith({
 			prettyPrint: {
